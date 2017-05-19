0909b93afaadb280fa269aaf708a918e8bbb4de0
js_park <jongsunday@naver.com>
2017-05-20 04:22:34 +0900

    DB_inser, MyLocation, DB_SELECT

--- app/src/main/java/material/kcci/mystudio/Fragment1.java
+++ app/src/main/java/material/kcci/mystudio/Fragment1.java
@@ -5,6 +5,14 @@ package material.kcci.mystudio;
  */
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,10 +20,21 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -31,17 +50,47 @@ public class Fragment1 extends Fragment {
 
     private String _intputDestName;
     private String searchValue;
+    private TextView myLocation;
+    private String _x,_y;
+
+    //region Chapter2
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+
+    MarkerOptions myLocationMarker;
+
+    private CompassView mCompassView;
+    private SensorManager mSensorManager;
+    private boolean mCompassEnabled;
+
     public Fragment1() {
         Log.d("TAG","Fragment1");
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
         final ViewGroup root_page = (ViewGroup) inflater.inflate(R.layout.fragment_fragment1,container,false);
 
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+                map.setMyLocationEnabled(true);
+            }
+        });
+
+        try {
+            MapsInitializer.initialize(getActivity());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        //endregion
+
+         //region onclick->DB_INSERT + 현재 위치 표시
         Button searchBtn = (Button) root_page.findViewById(R.id.button2);
         Log.d("onClick_TAG2","please");
         searchBtn.setOnClickListener(new View.OnClickListener()
@@ -57,6 +106,7 @@ public class Fragment1 extends Fragment {
 
                 Iterator<String> findMap = SavedMap.keySet().iterator();
 
+                //region while
                 while(findMap.hasNext())
                 {
                     String keys = (String) findMap.next();
@@ -68,20 +118,54 @@ public class Fragment1 extends Fragment {
                         insertToDatabase(_intputDestName,searchValue);
                     }
                 }
+                //endregion
 
+
+                myLocation = (TextView) root_page.findViewById(R.id.myTxtView);
+
+                requestMyLocation();
+
+
+                //region set
                 EditText input_destination = (EditText) root_page.findViewById(R.id.editText);
                 _intputDestName = input_destination.getText().toString();
                 Log.d("onClick_TAG1",_intputDestName);
                 TextView searchName = (TextView) root_page.findViewById(R.id.textView3);
                 TextView searchAddr = (TextView) root_page.findViewById(R.id.textView2);
                 Log.d("onClick_TAG3","searchClick_TAG");
+                myLocation.setText("현재 위치 :"+_x+_y);
                 searchName.setText(_intputDestName);
                 searchAddr.setText(searchValue);
+
+                //chapter2 액티비티 정의
+
+                //endregion
             }
         });
+         //endregion
+
+
+        mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        boolean sideBottom = true;
+        mCompassView = new CompassView(getActivity());
+        mCompassView.setVisibility(View.VISIBLE);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+        RelativeLayout.LayoutParams.WRAP_CONTENT,
+        RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        params.addRule(sideBottom ? RelativeLayout.ALIGN_PARENT_BOTTOM : RelativeLayout.ALIGN_PARENT_TOP);
+
+        ((ViewGroup)mapFragment.getView()).addView(mCompassView, params);
+
+
+
+
+        mCompassEnabled = true;
         return root_page;
     }
-
+    //이건 무조건 필요 (DB INSERT)
+    //region insertToDatabase
     private void insertToDatabase(String name,String addr){
 
         class InsertData extends AsyncTask<String, Void, String>
@@ -151,5 +235,143 @@ public class Fragment1 extends Fragment {
         InsertData task = new InsertData();
         task.execute(name,addr);
     }
-}
+    //endregion
 
+    //이건 필요 3시 19분
+    private void requestMyLocation() {
+        LocationManager manager =
+                (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            long minTime = 10000;
+            float minDistance = 0;
+            manager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    minTime,
+                    minDistance,
+                    new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            showCurrentLocation(location);
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+
+                        }
+                    }
+            );
+
+            Location lastLocation = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastLocation != null) {
+                showCurrentLocation(lastLocation);
+            }
+
+            manager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    minTime,
+                    minDistance,
+                    new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            showCurrentLocation(location);
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+
+                        }
+                    }
+            );
+
+
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //이건 필요 3시 19분
+    private void showCurrentLocation(Location location) {
+        LatLng curPoint = new LatLng(location.getLatitude(), location.getLongitude());
+
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+
+        showMyLocationMarker(location);
+    }
+
+    //이건 필요 3시 19분
+    private void showMyLocationMarker(Location location) {
+        if (myLocationMarker == null) {
+            myLocationMarker = new MarkerOptions();
+            myLocationMarker.position(new LatLng(location.getLatitude(), location.getLongitude()));
+            myLocationMarker.title("내 위치\n");
+            myLocationMarker.snippet("GPS로 확인한 위치");
+            myLocationMarker.icon(BitmapDescriptorFactory.fromResource(R.drawable.mylocation));
+            map.addMarker(myLocationMarker);
+        } else {
+            myLocationMarker.position(new LatLng(location.getLatitude(), location.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (map != null) {
+            map.setMyLocationEnabled(false);
+        }
+
+        if(mCompassEnabled) {
+            mSensorManager.unregisterListener(mListener);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (map != null) {
+            map.setMyLocationEnabled(true);
+        }
+
+        if(mCompassEnabled) {
+            mSensorManager.registerListener(mListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    private final SensorEventListener mListener = new SensorEventListener() {
+        private int iOrientation = -1;
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+
+        public void onSensorChanged(SensorEvent event) {
+            if (iOrientation < 0) {
+                iOrientation = ((WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
+            }
+            mCompassView.setAzimuth(event.values[0] + 90 * iOrientation);
+            mCompassView.invalidate();
+        }
+    };
+}
